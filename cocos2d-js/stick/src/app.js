ae9a7d03988ce85c1fc1f077d1b89cda9cdf0c69
builden <billdeng@qq.com>
2014-11-18 00:35:14 +0800

    更新背景移动动画

--- cocos2d-js/stick/src/app.js
+++ cocos2d-js/stick/src/app.js
@@ -3,7 +3,12 @@ var RUN_ANI = "run_ani";
 var HelloWorldLayer = cc.Layer.extend({
     sprite:null,
     step:0,     // 步数
-    vRc: null,
+    vRc: null,  // VisualRect
+    mountainHeight: 0, // 山体高度
+    startX: 120, // 起点位置
+    maxMountainWidth: 160,  // 最大山体宽度
+    minMountainWidth: 2,    // 最小山体宽度
+
 
     ctor:function () {
         //////////////////////////////
@@ -19,20 +24,33 @@ var HelloWorldLayer = cc.Layer.extend({
         this.vRc = cc.visibleRect;
         console.log(cc.formatStr("left {%d, %d}, bottomRight {%d, %d}",
             this.vRc.left.x, this.vRc.left.y, this.vRc.bottomRight.x, this.vRc.bottomRight.y));
+        this.mountainHeight = parseInt(this.vRc.height * 0.3);
+        console.log("montan height " + this.mountainHeight);
 
         this.initBg();
         this.initAni();
         this.runAni();
-        this.drawMountain(0, 20);
-        this.drawMountain(160, 40);
+
+        var initWidth = 80;
+        this.drawMountain(this.startX - initWidth, initWidth);
+        this.drawMountain(this.startX + 100, this.maxMountainWidth);
         this.drawStick();
         return true;
     },
 
     initBg: function() {
-        var bg = Ltc.exNode(new cc.Sprite(res.bg_jpg)).addTo_(this).pos_(this.vRc.center.x, this.vRc.center.y).
-            anchor_(0, 0.5);
-        bg.runAction(cc.moveBy(10, -1 * bg.width, 0));
+        var bg = Ltc.exNode(new cc.Sprite(res.bg_jpg)).addTo_(this).pos_(0, 0).
+            anchor_(0, 0);
+        var move = cc.moveBy(20, -1 * bg.width, 0);
+        var func = cc.callFunc(function() {
+
+        }, bg);
+        var seq = cc.sequence(move, func);
+        bg.runAction(seq);
+
+        var bg2 = new cc.Sprite(res.bg_jpg);
+        Ltc.exNode(bg2).addTo_(this).pos_(bg.width, 0).anchor_(0, 0);
+        bg2.runAction(seq.clone());
     },
 
     initAni: function() {
@@ -52,23 +70,24 @@ var HelloWorldLayer = cc.Layer.extend({
     },
 
     runAni: function() {
-        var sprite = Ltc.exNode(new cc.Sprite(res.run1_png)).pos_(100, 228).addTo_(this).scale_(0.5);
+        var sprite = new cc.Sprite(res.run1_png);
+        var x = this.startX - 24;
+        var y = this.mountainHeight + 24;
+        Ltc.exNode(sprite).pos_(x, y).addTo_(this).scale_(0.5);
         var animate = new cc.Animate(cc.animationCache.getAnimation(RUN_ANI));
         sprite.runAction(cc.repeatForever(animate));
     },
 
     drawMountain: function(pos, width) {
-//        var mountain = Ltc.exNode(new cc.DrawNode()).addTo_(this);
-//        var clr = cc.color.WHITE;
-//        mountain.setLineWidth(width);
-//        mountain.drawSegment(cc.p(pos, 40), cc.p(pos, 200), 1, clr);
-
-        var sprite = Ltc.exNode(new cc.Sprite(res.blank_png)).pos_(pos,0).addTo_(this).scale_(width, 200).
+        var sprite = Ltc.exNode(new cc.Sprite(res.blank_png)).pos_(pos,0).
+            addTo_(this).scale_(width, this.mountainHeight).
             anchor_(0, 0).color_(cc.color.RED);
+
+        sprite.runAction(cc.moveBy(3, -100, 0));
     },
 
     drawStick: function() {
-        var sprite = Ltc.exNode(new cc.Sprite(res.blank_png)).pos_(80,201).addTo_(this).scale_(1, 200).
+        var sprite = Ltc.exNode(new cc.Sprite(res.blank_png)).pos_(this.startX - 1, this.mountainHeight + 1).addTo_(this).scale_(1, 200).
             anchor_(0, 0).color_(cc.color.GREEN);
 
         var seq1 = cc.sequence(cc.rotateBy(1, 90), cc.delayTime(1), cc.scaleTo(1, 1, 40));
