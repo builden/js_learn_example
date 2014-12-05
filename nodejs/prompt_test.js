0489643a5a1b66babdb895672cada6287e31e0b4
builden <billdeng@qq.com>
2014-12-05 14:30:57 +0800

    增加prompt_test

---
+++ nodejs/prompt_test.js
@@ -0,0 +1,36 @@
+/**
+ * 输入提示
+ * @Author: Bill
+ * @Date:   2014-12-05 14:06:02
+ * @Last Modified by:   Bill
+ * @Last Modified time: 2014-12-05 14:17:11
+ */
+
+'use strict';
+
+var prompt = require('prompt');
+
+var properties = [{
+    name: 'username',
+    validator: /^[a-zA-Z\s\-]+$/,
+    warning: 'Username must be only letters, spaces, or dashes'
+}, {
+    name: 'password',
+    hidden: true
+}];
+
+prompt.start();
+
+prompt.get(properties, function(err, result) {
+    if (err) {
+        return onErr(err);
+    }
+    console.log('Command-line input received:');
+    console.log('  Username: ' + result.username);
+    console.log('  Password: ' + result.password);
+});
+
+function onErr(err) {
+    console.log(err);
+    return 1;
+}
