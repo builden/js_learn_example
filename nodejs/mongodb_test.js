70549da383360a2df00c44ccf910722c1453d98d
builden <billdeng@qq.com>
2014-12-09 18:59:12 +0800

    mongodb测试

---
+++ nodejs/mongodb_test.js
@@ -0,0 +1,38 @@
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://203.195.202.83/gameserver-dev', {db: {safe:true}});
+
+var GameSchema = new mongoose.Schema({
+    openid: { type: String, unique: true },
+    nick: String,
+    coin: {type: Number, default: 0},
+    // strength 体力
+    stren: {type: Number, default: 0},
+
+    // 当天第一次登录
+    dayFirstLogin: {type: Boolean, default: false},
+    // 头像url, 玩吧 50 x 50
+    faceurl: String,
+    // 道具
+    props: String,
+    // 周最高分数
+    weekScore: {type: Number, default: 0},
+    // 历史最高分数
+    highscore: {type: Number, default: 0},
+    // 上一次登录时间
+    lastLoginTime: Date,
+    // 上一次体力不为满值的时间，如果已经是满值，则为0
+    lastNoFullStrenTime: Date
+});
+
+var Game = mongoose.model('Game', GameSchema);
+
+var promise = Game.find({openid: {$in: ['49FC0B6F4AAB196C3ED50E6E28D4A61F']}}).exec();
+promise.then(
+    function(doc) {
+        console.log(doc);
+    },
+    function(err) {
+        console.log(err);
+    }
+)
