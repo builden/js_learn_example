d0276a6d0e4414cd8fc54d27e93b724dc5020d84
builden <billdeng@qq.com>
2014-12-11 00:09:00 +0800

    增加underscore.string测试用例

--- nodejs/string_test.js
+++ nodejs/string_test.js
@@ -2,7 +2,7 @@
  * @Author: dengtao
  * @Date:   2014-11-29 22:56:12
  * @Last Modified by:   dengtao
- * @Last Modified time: 2014-12-10 00:20:29
+ * @Last Modified time: 2014-12-11 00:08:11
  */
 
 'use strict';
@@ -25,6 +25,97 @@ function main() {
     // 编辑距离，是指两个字串之间，由一个转成另一个所需的最小编辑次数
     console.log(_s.levenshtein('kitten', 'kattan'));
     //=> 2
+
+    // 按固定长度分割
+    console.log(_s.chop('whitespace', 3));
+    //=> [ 'whi', 'tes', 'pac', 'e' ]
+
+    // 清除多余的空格
+    console.log(_s.clean(' foo    bar  '));
+    //=> foo bar
+
+    // 字符串拆成数组
+    console.log(_s.chars('hello'));
+    //=> [ 'h', 'e', 'l', 'l', 'o' ]
+
+    console.log(_s.swapCase('Hello'));
+    //=> hELLO
+
+    // 是否包含子串
+    console.log(_s.include('foobar', 'ob'));
+    //=> true
+
+    // 包含子串的个数
+    console.log(_s.count('foobar', 'o'));
+    //=> 2
+
+    // html标签转义, 相反则使用unescapeHTML
+    console.log(_s.escapeHTML('<div>Blah blah blah</div>'));
+    //=> &lt;div&gt;Blah blah blah&lt;/div&gt;
+
+    // 插入子串
+    console.log(_s.insert('hello', 2, 'insert'));
+    //=> heinsertllo
+
+    // 是否是空白串
+    console.log(_s.isBlank(' \n\t'));
+    //=> true
+
+    // _s._.join(separator, *strings)
+    console.log(_s.join('-', 'foo', 'bar', 'baz'));
+    //=> foo-bar-baz
+
+    // 按行拆分成字符串数组
+    console.log(_s.lines('hello\nworld'));
+    //=> [ 'hello', 'world' ]
+
+    console.log(_s.startsWith('image.gif', 'image'));
+    //=> true
+
+    console.log(_s.endsWith('image.gif', '.gif'));
+    //=> true
+
+    // ltrim, rtrim
+    console.log(_s.trim('_-foobar-_', '-_'));
+    //=> foobar
+
+    console.log(_s.truncate('hello world', 3));
+    //=> hel...
+
+    // 和prune类似，避免出现半个单词的情况
+    console.log(_s.prune('hello world', 4));
+    //=> ...
+
+    // 按正则表达式切割字符串，_s.words(str, delimiter=/\s+/)
+    console.log(_s.words('i-love-you', '-'));
+    //=> [ 'i', 'love', 'you' ]
+
+    console.log(_s.sprintf('%0.1f', 1.17));
+    //=> 1.2
+
+    console.log(_s.pad('1', 8, '0'));
+    //=> 00000001
+
+    console.log(_s.toNumber('2.556'));
+    //=> 3
+
+    // strLeft, strLeftBack
+    console.log(_s.strRight('This_is_a_test_string', '_'));
+    //=> is_a_test_string
+    console.log(_s.strRightBack('This_is_a_test_string', '_'));
+    //=> string
+
+    console.log(_s.repeat('foo', 3));
+    //=> foofoofoo
+
+    console.log(_s.surround('foo', 'ab'));
+    //=> abfooab
+
+    console.log(_s.quote('foo'));
+    //=> "foo"
+
+    console.log(_s.unquote('"foo"'));
+    //=> foo
 }
 
 /**
