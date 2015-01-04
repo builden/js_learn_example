390b2f8fb437d636598c5807ffdd7c7126dc367a
builden <billdeng@qq.com>
2015-01-04 19:20:25 +0800

    增加数据库测试用例

--- nodejs/mongodb_test.js
+++ nodejs/mongodb_test.js
@@ -1,67 +1,244 @@
+/*
+ * @Author: Bill
+ * @Date:   2014-10-14 14:14:56
+ * @Last Modified by:   Bill
+ * @Last Modified time: 2015-01-04 18:54:46
+ *
+ * http://mongoosejs.com/
+ */
+
+
 var mongoose = require('mongoose');
+var db = mongoose.connection;
 var async = require('async');
 var _ = require('lodash');
 var moment = require('moment');
 
-var dbHost = 'mongdodb://localhost/forTest';
-var testSchema = {
-    id: {type: Number, unique: true},
-    createTime: {type: Date, default: Date.now}
+function main() {
+    var testModel = initModel();
+    // testModel.remove().exec(function(err, result) {
+    //     initInsert(testModel);
+    // });
+    // createTest(testModel);
+    findTest(testModel);
+    // removeTest(testModel);
+    // updateTest(testModel);
+    // incrementVersion(testModel);
+
+    // 统计数量
+    // count(condition, function(err, count) {})
 }
 
-function main() {
-    var testModel = mongoose.model('ForTest', testSchema);
+function initModel() {
+    mongoose.set('debug', true);
 
-    // insert
-    var info = {
-        id: _.random(10)
-    };
+    // connect mongodb
+    // mongodb://user:pass@localhost/...
+    var dbHost = 'mongodb://localhost/forTest';
+    mongoose.connect(dbHost);
 
-    testModel.Create(info, function(err, doc) {
-        if (err) {
-            console.log('insert error ' + err);
-        } else {
-            console.log('insert rst ' + doc);
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', function(callback) {
+        console.log('one connect opened');
+    });
+
+    // Define Schema
+    var testSchema = mongoose.Schema({
+        id: {
+            type: Number,
+            unique: true
+        },
+        name: String,
+        createTime: {
+            type: Date,
+            default: Date.now
         }
-    })
+    });
+
+    // Define Model
+    var testModel = mongoose.model('ForTest', testSchema);
+    return testModel;
+}
+
+function initInsert(testModel) {
+    for (var i = 0; i < 10; i++) {
+        var one = new testModel({
+            id: i,
+            name: 'name' + i
+        });
+        one.save(function(err, doc) {
+            if (err) {
+                console.error('save err: ' + err);
+            }
+        });
+    }
+}
+
+function createTest(testModel) {
+    // 插入
+    var insertOne = new testModel({
+        id: _.random(10),
+        name: 'name' + _.random(1, 10)
+    });
+
+    insertOne.save(function(err, doc) {
+        if (err) {
+            console.error('save error: ' + err);
+        } else {
+            console.log('=========== insert one ==========');
+            console.log(doc);
+        }
+    });
+}
+
+function findTest(testModel) {
+    // find([criteria], [callback])
+    // 查找所有，限制取前3个
+    // .skip(3)，跳过前3个，取剩下的
+    // sort({name: 1, id: -1}); // 1是升序，-1是降序 <==> sort('name -id')
+    testModel.find().limit(3).sort('-id').exec(function(err, infos) {
+        if (err) {
+            console.error('find error: ' + err);
+        } else {
+            console.log('======== find all =========');
+            console.log(infos);
+        }
+    });
+
+    // 根据条件查找，根据正则表达式查找String字段
+    /*    testModel.find({
+            name: /[0-1]$/
+        }, function(err, infos) {
+            if (err) {
+                console.error('find error: ' + err);
+            } else {
+                console.log('========= find with condition =======');
+                console.log(infos);
+            }
+        });*/
+
+    // 查找第一个满足条件的document
+    /*    testModel.findOne({
+            name: /[0-1]$/
+        }, function(err, info) {
+            if (err) {
+                console.error('find error: ' + err);
+            } else {
+                console.log('========= find one with condition =======');
+                console.log(info);
+            }
+        });*/
+
+    // 根据objecId查找指定document
+    /*    testModel.findById('54a8de555854848416ed7d0c', function(err, info) {
+            if (err) {
+                console.error('find error: ' + err);
+            } else {
+                console.log('========= findById =======');
+                console.log(info);
+            }
+        });*/
+
+    // 根据条件查找
+    // <==> testModel.where('id').gte(0).lte(4).find(callback);
+    // <==> testModel.find().gte('id', 0).lte('id', 4).exec(callback);
+    // gte -- 大于等于
+    // lte -- 小于等于
+    // gt  -- 大于
+    // lt  -- 小于
+    // ne  -- 不等于
+    // in  -- 单一键，在数组内的值 find({id: {$in: [1, 3, 5]}});
+    // nin -- 单一键，不在数组内的值
+    // or  -- 多个键，只需要一个键满足 find({$or: [{id: 3}, {name: '123'}]});
+    // mod -- 取模 find({id: {$mod: [5, 1]}}); // 模5余1
+    // not -- 取反 find({id: {$not: {$mod: [5, 1]}}});
+    // testModel.where('id').gt(1).lte(4).find(function(err, infos) {
+    testModel.find({
+        id: {
+            $gte: 1,
+            $lte: 4
+        }
+    }, function(err, infos) {
+        if (err) {
+            console.error('find error: ' + err);
+        } else {
+            console.log('========= find with where =======');
+            console.log(infos);
+        }
+    });
+}
+
+function removeTest(testModel) {
+    // remove([criteria], [callback])
+    testModel.remove({
+        id: {
+            $gte: 1,
+            $lte: 4
+        }
+    }, function(err, num) {
+        if (err) {
+            console.error('remove error: ' + err);
+        } else {
+            console.log('========= remove with where =======');
+            console.log('removed ' + num);
+        }
+    });
+
+    // 删除单个
+    // Model.findOneAndRemove(conditions, [options], [callback])
+}
+
+function updateTest(testModel) {
+    // 方式1 - 先查找，后修改
+    /*    async.waterfall([function(cb) {
+            testModel.findOne({
+                id: 3
+            }).exec(cb);
+        }, function(doc, cb) {
+            if (doc) {
+                doc.name = '321';
+                doc.save(cb);
+            } else {
+                cb('have not find doc');
+            }
+        }], function(err, doc) {
+            if (err) {
+                console.error('1.1 update err: ' + err);
+            } else {
+                console.log('====== update result =====');
+                console.log(doc);
+            }
+        });*/
+
+    // 方式二，直接调用update方法
+    // Model.update(conditions, update, [options], [callback])
+    testModel.update({
+        id: 4
+    }, {
+        name: '4567'
+    }).exec(function(err, doc) {
+        if (err) {
+            console.error('1.2 update err: ' + err);
+        } else {
+            console.log('====== update result =====');
+            console.log(doc);
+        }
+    });
+
+    // 方式三
+    // Model.findOneAndUpdate([conditions], [update], [options], [callback])
+}
+
+function incrementVersion(testModel) {
+    // 修改__v字段
+    testModel.findOne({
+        id: 1
+    }).exec(function(err, doc) {
+        doc.increment();
+        doc.save(function(err, doc) {
+
+        })
+    });
 }
 
 main();
-
-/*
-mongoose.connect('mongodb://203.195.202.83/gameserver-dev', {db: {safe:true}});
-
-var GameSchema = new mongoose.Schema({
-    openid: { type: String, unique: true },
-    nick: String,
-    coin: {type: Number, default: 0},
-    // strength 体力
-    stren: {type: Number, default: 0},
-
-    // 当天第一次登录
-    dayFirstLogin: {type: Boolean, default: false},
-    // 头像url, 玩吧 50 x 50
-    faceurl: String,
-    // 道具
-    props: String,
-    // 周最高分数
-    weekScore: {type: Number, default: 0},
-    // 历史最高分数
-    highscore: {type: Number, default: 0},
-    // 上一次登录时间
-    lastLoginTime: Date,
-    // 上一次体力不为满值的时间，如果已经是满值，则为0
-    lastNoFullStrenTime: Date
-});
-
-var Game = mongoose.model('Game', GameSchema);
-
-var promise = Game.find({openid: {$in: ['49FC0B6F4AAB196C3ED50E6E28D4A61F']}}).exec();
-promise.then(
-    function(doc) {
-        console.log(doc);
-    },
-    function(err) {
-        console.log(err);
-    }
-)*/
--- nodejs/regex_test.js
+++ nodejs/regex_test.js
@@ -1,32 +1,37 @@
-var str = "Mozilla/5.0 (Linux; Android   4.3; Nexus 7 Build/JSS15Q) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.72 Safari/537.36";
+/**
+ * 正则表达式测试
+ * @Author: Bill
+ * @Date:   2014-11-20 13:50:44
+ * @Last Modified by:   Bill
+ * @Last Modified time: 2015-01-04 14:26:51
+ */
 
-// var str = "'lenovo-lenovo-a288t/1.0 linux/2.6.35.7 android/2.3.5 release/08.01.2012 browser/applewebkit533.1 (khtml, like gecko) mozilla/5.0 mobile'";
+var assert = require('assert');
 
-var st='<img alt="werwer" src="werwer.jpg"  title="">'
-// var r = st.match(/src=\"([^\"]*)\"/);
-// console.log(r);
-
-// console.log(r[1]);
-
-// var r2 = str.match(/Android([^\.]*)/i);
-// if (r2) {
-//     console.log(r2);
-//     var rst = r2[1];
-//     console.log(rst);
-//     rst = rst.replace(/^\s*/, "").replace(/^\//, "");
-//     console.log(rst);
-//     // console.log(parseInt(r2[1].split('.')[0]));
-// } else {
-//     console.log("not match");
-// }
-
-
-var m = str.match(/Android([^\.]*)/i);
-var majorVer = m[1];
-majorVer = parseInt(majorVer.replace(/^\s*/, "").replace(/^\//, ""));
-console.log(majorVer);
-if (majorVer <= 2) {
-    console.log("is match");
-} else {
-    console.log("not match");
+function main() {
+    // matchAndroidVerTest();
+    numberTest();
 }
+
+function numberTest() {
+    var str = '5';
+    assert(str.match(/[0-4]/));
+}
+
+function matchAndroidVerTest() {
+    var str = "Mozilla/5.0 (Linux; Android   4.3; Nexus 7 Build/JSS15Q) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.72 Safari/537.36";
+
+    // var str = "'lenovo-lenovo-a288t/1.0 linux/2.6.35.7 android/2.3.5 release/08.01.2012 browser/applewebkit533.1 (khtml, like gecko) mozilla/5.0 mobile'";
+
+    var m = str.match(/Android([^\.]*)/i);
+    var majorVer = m[1];
+    majorVer = parseInt(majorVer.replace(/^\s*/, "").replace(/^\//, ""));
+    console.log(majorVer);
+    if (majorVer <= 2) {
+        console.log("is match");
+    } else {
+        console.log("not match");
+    }
+}
+
+main();
