d33bce86ca3b7dc21e9bdda8f12efbfe76cf3814
builden <billdeng@qq.com>
2015-05-20 00:17:34 +0800

    增加模板、增加help提示

--- nodejs/own_tools/npmg/cli.js
+++ nodejs/own_tools/npmg/cli.js
@@ -4,6 +4,8 @@ var fs = require('fs-extra');
 var argv = require('optimist').argv;
 var opener = require('opener');
 var s = require('underscore.string');
+var path = require('path');
+var _ = require('lodash');
 
 var globalPath = null;
 if (process.platform === 'win32') {
@@ -13,11 +15,32 @@ if (process.platform === 'win32') {
 
 var cmd = [
   '@IF EXIST "%~dp0\\node.exe" (',
-  '  "%~dp0\\node.exe"  "%~dp0\\node_modules\\http-server\\bin\\http-server" %*',
+  '  "%~dp0\\node.exe"  "<%= path %>" %*',
   ') ELSE (',
   '  @SETLOCAL',
   '  @SET PATHEXT=%PATHEXT:;.JS;=;%',
-  '  node  "%~dp0\node_modules\http-server\bin\http-server" %*',
+  '  node  "<%= path %>" %*',
   ')'
 ].join('\n');
-console.log(cmd);
+var compiled = _.template(cmd);
+console.log(compiled({
+  path: __filename
+}));
+
+if (argv.h || argv.help) {
+  console.log([
+    'usage: npmg [options]',
+    '',
+    'options:',
+    '  -s --self        register self to system path',
+    '  -n --name        global name',
+    '  -j --js          dest js file path',
+    '  -h --help        print the help list'
+    ].join('\n'));
+}
+
+if (argv.s || argv.self) {
+
+}
+
+
